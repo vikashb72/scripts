4e46bc543b5657616e50ecadb86ad589f57d5ed5
Vikash Badal <vikashb@where-ever.za.net>
2024-08-28 12:31:19 +0200

    add script to store tls in external vault

---
+++ minikube/gitops/store-vault-certs-external-vault.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export VAULT_K8S_NAMESPACE="vault"
+export WORKDIR="$(pwd)/work/tls"
+export VAULT_ADDR=$EXTERNAL_VAULT_ADDR
+export ENV='minikube'
+
+kubectl -n $VAULT_K8S_NAMESPACE get secret vault-ha-tls -o json | \
+    jq -r '.data | to_entries | map({key: .key, value: (.value | @base64d)}) | from_entries' \
+    > ${WORKDIR}/vault-ha-tls.json
+
+export VAULT_ADDR=$EXTERNAL_VAULT_ADDR
+vault login $EXTERNAL_VAULT_TOKEN || exit
+
+vault kv put kv/${ENV}/vault/tls @${WORKDIR}/vault-ha-tls.json
+vault kv get kv/${ENV}/vault/tls
